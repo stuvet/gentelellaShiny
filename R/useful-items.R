3a0474a491c8d7bfc9c5ef884b665b03da3302c5
Stuart Russell <stuartrussell@nantwichfarmvets.co.uk>
2019-10-22 17:30:08 +0100

    Update useful-items.R

--- R/useful-items.R
+++ R/useful-items.R
@@ -929,22 +929,22 @@ tileCountElement <- function(id = '', value = 2500, change_value = "4%", going_w
                var tile_top = tile.find('.count_top');
                var tile_delta = tile_bottom.find('#' + obj.id + '_delta');
                if(obj.hasOwnProperty('tile_title')){
-                  tile_top.contents().filter(function(){ return this.nodeType == 3; }).replaceWith(obj.tile_title);
+                  tile_top.contents().filter(function(){ return this.nodeType == 3; }).last().replaceWith(obj.tile_title);
                }
                if(obj.hasOwnProperty('icon_text')){
                   tile_top.find('.fa').attr('class', 'fa ' + obj.icon_text);
                }
                if(obj.hasOwnProperty('value')){
-                  tile_top.find('div').contents().filter(function(){ return this.nodeType == 3; }).replaceWith(obj.value);
+                  tile.find('div').contents().filter(function(){ return this.nodeType == 3; }).last().replaceWith(obj.value);
                }
                if(obj.hasOwnProperty('from_text')){
-                  tile_bottom.contents().filter(function(){ return this.nodeType == 3; }).replaceWith(obj.from_text);
+                  tile_bottom.contents().filter(function(){ return this.nodeType == 3; }).last().replaceWith(obj.from_text);
                }
                if(obj.hasOwnProperty('colour')){          
                   tile_delta.attr('class', obj.colour);
                }
                if(obj.hasOwnProperty('delta')){          
-                  tile_delta.contents().filter(function(){ return this.nodeType == 3; }).replaceWith(obj.delta);
+                  tile_delta.contents().filter(function(){ return this.nodeType == 3; }).last().replaceWith(obj.delta);
                }
                if(obj.hasOwnProperty('direction')){          
                  if(obj.direction === 'up') {
